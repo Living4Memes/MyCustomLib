8bb010616e667c4be80e632a45e30a5502f28466
Living4Memes_WORK <destroyvlad@gmail.com>
2022-06-30 16:53:58 +0300

    Добавил документацию EnhancedImage

--- MyCustomLib/GraphicFunctions/EnhancedImage.cs
+++ MyCustomLib/GraphicFunctions/EnhancedImage.cs
@@ -10,22 +10,56 @@ using MyCustomLib.Controls;
 
 namespace MyCustomLib.GraphicFunctions
 {
+      /// <summary>
+      /// Изображение с эффектами
+      /// </summary>
       public class EnhancedImage
       {
             private bool _drawBorder = false;
             private bool _drawShadow = false;
 
+            /// <summary>
+            /// Изображение без эффектов
+            /// </summary>
             public Image PlainImage { get; private set; }
+            /// <summary>
+            /// Изображение с рамкой
+            /// </summary>
             public Image ImageWithBorder { get; private set; }
+            /// <summary>
+            ///  Изображение с тенью
+            /// </summary>
             public Image ShadowedImage { get; private set; }
+            /// <summary>
+            /// Изображение с тенью и рамкой
+            /// </summary>
             public Image ShadowedImageWithBorder { get; private set; }
+            /// <summary>
+            /// Настройки контейнера, в котором находится изображение
+            /// </summary>
             public CustomContainerProperties ContainerProperties { get; private set; }
+            /// <summary>
+            /// Настройки тени
+            /// </summary>
             public ShadowProperties ShadowProperties { get; private set; }
+            /// <summary>
+            /// Настройки рамки
+            /// </summary>
             public BorderProperties BorderProperties { get; private set; }
 
+            /// <summary>
+            /// Включение и выключение рамки
+            /// </summary>
             public bool DrawBorder { get => _drawBorder; set { _drawBorder = value; Initialize(); } }
+            /// <summary>
+            /// Включение и выключение тени
+            /// </summary>
             public bool DrawShadow { get => _drawShadow; set { _drawShadow = value; Initialize(); } }
 
+            /// <summary>
+            /// Инициализирует новый экземпляр класса <see cref="EnhancedImage"/> через интерфейс
+            /// </summary>
+            /// <param name="enhancedPlainImage">Интерфейс, на основе которого инициализируется экземпляр класса</param>
             public EnhancedImage(IEnhancedPlainImage enhancedPlainImage)
             {
                   PlainImage = enhancedPlainImage.PlainImage;
@@ -34,6 +68,10 @@ namespace MyCustomLib.GraphicFunctions
                   Initialize();
             }
 
+            /// <summary>
+            /// Инициализирует новый экземпляр класса <see cref="EnhancedImage"/> через интерфейс
+            /// </summary>
+            /// <param name="enhancedPlainImageWithBorder">Интерфейс, на основе которого инициализируется экземпляр класса</param>
             public EnhancedImage(IEnhancedPlainImageWithBorder enhancedPlainImageWithBorder)
             {
                   PlainImage = enhancedPlainImageWithBorder.PlainImage;
@@ -45,6 +83,10 @@ namespace MyCustomLib.GraphicFunctions
                   Initialize();
             }
 
+            /// <summary>
+            /// Инициализирует новый экземпляр класса <see cref="EnhancedImage"/> через интерфейс
+            /// </summary>
+            /// <param name="enhancedImageWithShadow">Интерфейс, на основе которого инициализируется экземпляр класса</param>
             public EnhancedImage(IEnhancedImageWithShadow enhancedImageWithShadow)
             {
                   PlainImage = enhancedImageWithShadow.PlainImage;
@@ -56,6 +98,10 @@ namespace MyCustomLib.GraphicFunctions
                   Initialize();
             }
 
+            /// <summary>
+            /// Инициализирует новый экземпляр класса <see cref="EnhancedImage"/> через интерфейс
+            /// </summary>
+            /// <param name="enhancedImageWithShadowAndBorder">Интерфейс, на основе которого инициализируется экземпляр класса</param>
             public EnhancedImage(IEnhancedImageWithShadowAndBorder enhancedImageWithShadowAndBorder)
             {
                   PlainImage = enhancedImageWithShadowAndBorder.PlainImage;
@@ -69,8 +115,14 @@ namespace MyCustomLib.GraphicFunctions
                   Initialize();
             }
 
+            /// <summary>
+            /// Асинхронная инициализация <see cref="EnhancedImage"/>
+            /// </summary>
             protected async void InitializeAsync() => await Task.Run(Initialize); 
 
+            /// <summary>
+            /// Инициализация <see cref="EnhancedImage"/>
+            /// </summary>
             protected void Initialize()
             {
                   if (_drawBorder)
@@ -84,6 +136,10 @@ namespace MyCustomLib.GraphicFunctions
 
             }
 
+            /// <summary>
+            /// Устанавливает толщину рамки <see cref="ImageWithBorder"/> и <see cref="ShadowedImageWithBorder"/>
+            /// </summary>
+            /// <param name="borderWidth">Толщина рамки</param>
             public void SetBorderWidth(float borderWidth)
             {
                   BorderProperties = new BorderProperties()
@@ -95,6 +151,10 @@ namespace MyCustomLib.GraphicFunctions
                   Initialize();
             }
 
+            /// <summary>
+            /// Устанавливает цвет рамки <see cref="ShadowedImage"/> и <see cref="ShadowedImageWithBorder"/>
+            /// </summary>
+            /// <param name="borderColor">Толщина рамки</param>
             public void SetBorderColor(Color borderColor)
             {
                   BorderProperties = new BorderProperties()
